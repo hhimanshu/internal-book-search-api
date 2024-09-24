07513551a7dfab851d10f2d5e49b21c9cd48a108
hhimanshu <dev.atwork@yahoo.com>
2024-09-25 11:02:58 +1200

    making test work

---
+++ src/test/java/com/h2/AppTest.java
@@ -0,0 +1,12 @@
+package com.h2;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+
+    @Test
+    public void testApp() {
+        assertEquals(1, 1);
+    }
+}
